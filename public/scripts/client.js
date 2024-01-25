463c34755b1c6e651b51426028f371537f6e3a65
Kai Wingfield <kaiwingfield17@gmail.com>
2024-01-25 11:38:39 -0800

    Implement form validation. refetch tweets on submission. Add protective measures against cross-site-scripting.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
 
-const createTweetElement = function (tweetObject) {
-  const $tweet = $(`
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  const createTweetElement = function(tweetObject) {
+    const $tweet = $(`
   <article class="tweet">
   <header>
     <div>
@@ -12,7 +18,7 @@ const createTweetElement = function (tweetObject) {
       <h3 class="handle">${tweetObject.user.handle}</h3>
     </div>
   </header>
-  <p class="tweet-body">${tweetObject.content.text}</p>
+  <p class="tweet-body">${escape(tweetObject.content.text)}</p>
   <footer>
       <output>${timeago.format(tweetObject.created_at)}</output>
       <span>
@@ -26,59 +32,68 @@ const createTweetElement = function (tweetObject) {
   </footer>
 </article>
 `);
-return $tweet;
+    return $tweet;
 
-};
+  };
 
-const renderTweets = function(tweetsArray) {
-  $('#tweets-container').empty();
-  for(const tweet of tweetsArray) {
-    let $tweet = createTweetElement(tweet);
-    $('#tweets-container').append($tweet);
-  }
-}
+  const loadTweets = function() {
 
-// Event listener for form submission
-$("#tweet-text-form").on("submit", function(event) {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json',
+      success: function(data) {
+        console.log("Recieved Tweets: ", data);
+        renderTweets(data);
+      },
+      error: function(error) {
+        console.log('Error loading tweets: ', error);
+      }
+    });
 
+  };
+
+  const renderTweets = function(tweetsArray) {
+    $('#tweets-container').empty();
+    for (const tweet of tweetsArray) {
+      let $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }
+  };
+
+  // Event listener for form submission
+  $("#tweet-text-form").on("submit", function(event) {
   // Prevents default form submission
-  event.preventDefault();
-  // serialize the data
-  const serializedTweet = $("#tweet-text-form").serialize();
+    event.preventDefault();
+    // serialize the data
+    const serializedTweet = $("#tweet-text-form").serialize();
+    //Error handling/ validation
+    if (serializedTweet.length > 140) {
+      alert("Tweet is to long, please shorten");
+    } else if (serializedTweet === null) {
+      alert("Cannot post empty Tweet");
+    } else {
+ 
 
-  // Subit a POST request to the server
-  $.ajax({
-    method: "POST",
-    url: "/tweets",
-    data: serializedTweet,
-    success: function(result){
-      console.log("Tweet was posted successfully");
-    },
-    error: function(err){
-      console.log("There was an error ",err);
+      // Subit a POST request to the server
+      $.ajax({
+        method: "POST",
+        url: "/tweets",
+        data: serializedTweet,
+        success: function(result) {
+          console.log("Tweet was posted successfully");
+        },
+        error: function(err) {
+          console.log("There was an error ",err);
+        }
+      });
+      loadTweets();
+      document.getElementById("tweet-text-form").reset();
     }
   });
 
-});
 
-const loadTweets = function() {
-
-  $.ajax({
-    url: '/tweets',
-    method: 'GET',
-    dataType: 'json',
-    success: function(data) {
-      console.log("Recieved Tweets: ", data);
-      renderTweets(data);
-    },
-    error: function(error) {
-      console.log('Error loading tweets: ', error);
-    }
-  })
-
-};
-
-loadTweets();
+  loadTweets();
 });
 
 
