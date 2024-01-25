9920359f263a88971cd1eeb1febcc06785dd3c6f
Kai Wingfield <kaiwingfield17@gmail.com>
2024-01-24 17:41:26 -0800

    Add ajax GET request to request and display tweet array from the server database at /tweets. Implement timeago for tweet times

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,53 +1,27 @@
 $(document).ready(function() {
 
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1706135429765
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1706135363149
-    }
-]
-
 // converts timestamp to a readable string
-const timestampToReadable = function(timestamp) {
-  const currentTimestamp = Math.floor(Date.now() / 1000); //convert to seconds
-  const tweetTime = Math.floor(timestamp / 1000); //convert to seconds
-  const timeDifference = currentTimestamp - tweetTime;
+// const timestampToReadable = function(timestamp) {
+//   const currentTimestamp = Math.floor(Date.now() / 1000); //convert to seconds
+//   const tweetTime = Math.floor(timestamp / 1000); //convert to seconds
+//   const timeDifference = currentTimestamp - tweetTime;
 
-  if (timeDifference < 60){
-    return "just now";
-  } else if (timeDifference < 3600){
-    const minutesAgo = Math.floor(timeDifference / 60);
-        return `${minutesAgo} ${minutesAgo === 1 ? 'minute' : 'minutes'} ago`;
-  } else if (timeDifference < 86400) {
-     const hoursAgo = Math.floor(timeDifference / 3600);
-     return `${hoursAgo} ${hoursAgo === 1 ? 'hour' : 'hours'} ago`;
-  } else {
-     const daysAgo = Math.floor(timeDifference / 86400);
-     return `${daysAgo} ${daysAgo === 1 ? 'day' : 'days'} ago`;
- }
+//   if (timeDifference < 60){
+//     return "just now";
+//   } else if (timeDifference < 3600){
+//     const minutesAgo = Math.floor(timeDifference / 60);
+//         return `${minutesAgo} ${minutesAgo === 1 ? 'minute' : 'minutes'} ago`;
+//   } else if (timeDifference < 86400) {
+//      const hoursAgo = Math.floor(timeDifference / 3600);
+//      return `${hoursAgo} ${hoursAgo === 1 ? 'hour' : 'hours'} ago`;
+//   } else {
+//      const daysAgo = Math.floor(timeDifference / 86400);
+//      return `${daysAgo} ${daysAgo === 1 ? 'day' : 'days'} ago`;
+//   }
 
-}
+// }
 
 const createTweetElement = function (tweetObject) {
-const timestamp = timestampToReadable(tweetObject.created_at)
   const $tweet = $(`
   <article class="tweet">
   <header>
@@ -61,7 +35,7 @@ const timestamp = timestampToReadable(tweetObject.created_at)
   </header>
   <p class="tweet-body">${tweetObject.content.text}</p>
   <footer>
-      <output>${timestamp}</output>
+      <output>${timeago.format(tweetObject.created_at)}</output>
       <span>
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -105,14 +79,28 @@ $("#tweet-text-form").on("submit", function(event) {
       console.log("There was an error ",err);
     }
   });
-  // console.log(serializedTweet);
-  // console.log("In the event handler")
+
 });
 
-renderTweets(data);
+const loadTweets = function() {
+
+  $.ajax({
+    url: '/tweets',
+    method: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      console.log("Recieved Tweets: ", data);
+      renderTweets(data);
+    },
+    error: function(error) {
+      console.log('Error loading tweets: ', error);
+    }
+  })
+
+};
+
+loadTweets();
 });
 
-// http://localhost:8080/tweets - POST (Send a payload of tweet to the Server and say please save this!)
 
-// http://localhost:8080/tweets - GET
 
